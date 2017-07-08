cd4cfe85442e1f1ee9e82609870a990b7c6af4f0
Suhair Zain <suhair@qplaytech.com>
2017-07-08 12:04:52 +0530

    Fixed code to match expectation1

--- index.js
+++ index.js
@@ -1,37 +1,39 @@
-const slow1 = (a, b) => {
-    console.log('CALLED 1 with', a, b);
-    return a + b + 1;
+const slow1 = (...params) => {
+    console.log('CALLED 1');
+    return params.reduce((a, b) => a + b) + 1;
 };
 
-const slow2 = (a, b) => {
-    console.log('CALLED 2 with', a, b);
-    return a + b + 2;
+const slow2 = (...params) => {
+    console.log('CALLED 2');
+    return params.reduce((a, b) => a + b) + 2;
 };
 
-const slow3 = (a, b) => {
-    console.log('CALLED 3 with', a, b);
-    return a + b + 3;
+const slow3 = (...params) => {
+    console.log('CALLED 3');
+    return params.reduce((a, b) => a + b) + 3;
 };
 
-const slow4 = (a, b) => {
-    console.log('CALLED 4 with', a, b);
-    return a + b + 4;
+const slow4 = (...params) => {
+    console.log('CALLED 4');
+    return params.reduce((a, b) => a + b) + 4;
 };
 
 
 const resultAcc = {
 };
 const mySlow = (...funcs) => (...params) => {
-    const key = `a${params.sort().map((a) => `${a},`)}`;
+    const key = `a${params.sort().map((a) => `${a}-`).join('')}`;
 
     if (resultAcc[key] === undefined) {
-        let result = funcs.reduce((acc, curr) => {
+        resultAcc[key] = funcs.reduce((acc, curr) => {
             return () => curr(acc(...params), ...params);
-        });
-        resultAcc[key] = result;
+        })();
     }
     return resultAcc[key];
 };
 
 
-console.log(mySlow(slow1, slow2, slow3, slow4)(1, 2)());
+console.log(mySlow(slow1, slow2, slow3, slow4)(1, 2));
+console.log(mySlow(slow1, slow2, slow4, slow3)(2, 1));
+console.log(mySlow(slow1, slow2, slow3, slow4)(1, 2, 3));
+console.log(mySlow(slow1, slow2, slow3, slow4)(3, 1, 2));
